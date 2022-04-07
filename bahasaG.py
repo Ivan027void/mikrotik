b4adaaa03c674b109af897506c32228b5f6801d9
raihan <101862582+raishabirah@users.noreply.github.com>
2022-04-07 15:14:53 +0700

    Update bahasaG.py

--- bahasaG.py
+++ bahasaG.py
@@ -1,1 +1,11 @@
 #konversi string ke bahasa G tahun 90an
+print("Masukan Kalimat : ")
+txt = input()
+
+def bahasaG(txt):
+  dictionary = {'a': 'aga', 'e':'ege', 'i': 'igi', 'o': 'ogo', 'u': 'ugu', 'A': 'Aga', 'E':'Ege', 'I': 'Igi', 'O': 'Ogo', 'U': 'Ugu'}
+  transTable = txt.maketrans(dictionary)
+  txt = txt.translate(transTable)
+  print(txt)
+  
+bahasaG(txt)
