7b8c82387b83eff74753b56130a63f21203abb70
Ivan027void <100928027+Ivan027void@users.noreply.github.com>
2022-04-18 12:05:36 +0700

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -1,3 +1,9 @@
 #no3 proyek pemrograman
 #segitiga dengan python
-
+ 
+rows=input('masukkan jumlah baris:' )
+rows=int(rows)
+for row in range(1, rows+1):
+    for column in range(1, row + 1):
+        print(column, end=' ')
+    print("")
