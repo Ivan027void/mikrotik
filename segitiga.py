499b03d11d962424dbc9929cd583042889e4763a
IchwanulFata <101862614+IchwanulFata@users.noreply.github.com>
2022-04-18 15:41:33 +0700

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -1,9 +1,16 @@
-#no3 proyek pemrograman
-#segitiga dengan python
+#No.3 Proyek Pemrograman
+#Piramida angka Segitiga dengan python
  
-rows=input('masukkan jumlah baris:' )
-rows=int(rows)
-for row in range(1, rows+1):
-    for column in range(1, row + 1):
-        print(column, end=' ')
-    print("")
+baris = int(input("Masukkan Jumlah Baris: "))
+koef = 1
+
+for i in range(1, baris+1):
+    for space in range(1, baris-i+1):
+        print(" ",end="")
+    for j in range(0, i):
+        if j==0 or i==0:
+            koef = 1
+        else:
+            koef = koef + 1
+        print(koef, end = " ")
+    print()
