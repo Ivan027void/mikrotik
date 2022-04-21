a9c6340f0f44f4fcb7e926a9cdff083efb2eb56c
Wildafahera03 <101865160+Wildafahera03@users.noreply.github.com>
2022-04-21 11:04:53 +0700

    Update pengurutan.py

--- pengurutan.py
+++ pengurutan.py
@@ -1,1 +1,21 @@
 #no 4 proyek pemrograman B
+# membuat daftar kosong
+list = []
+
+# jumlah elemen sebagai input (jumlah elemen adalah banyak angka yang akan diurutkan)
+n = int(input("Masukkan jumlah elemen : "))
+
+# iterasi sampai kisaran
+for i in range(0, n):
+	ele = int(input())
+
+	list.append(ele) # menambahkan elemen sebanyak diinput pada n
+#data sebelum diurutkan menampilkan data yang sama dengan yang diinput
+print("data sebelum diurutkan:") 
+print( list)
+
+#data sesudah diurutkan menampilkan data urut 
+print("data setelah diurutkan:") 
+print(sorted(list))
+© 2022 GitHub, Inc.
+Terms
