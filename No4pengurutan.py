66d67b9a2e655b30f3c5fbf4ea74f6003686968a
Ivan027void <100928027+Ivan027void@users.noreply.github.com>
2022-04-21 10:53:47 +0700

    Update No4pengurutan.py

--- No4pengurutan.py
+++ No4pengurutan.py
@@ -1,15 +1,18 @@
 # membuat daftar kosong
 list = []
 
-# jumlah elemen sebagai input
+# jumlah elemen sebagai input (jumlah elemen adalah banyak angka yang akan diurutkan)
 n = int(input("Masukkan jumlah elemen : "))
 
 # iterasi sampai kisaran
 for i in range(0, n):
 	ele = int(input())
 
-	list.append(ele) # menambahkan elemen
+	list.append(ele) # menambahkan elemen sebanyak diinput pada n
+#data sebelum diurutkan menampilkan data yang sama dengan yang diinput
 print("data sebelum diurutkan:") 
 print( list)
+
+#data sesudah diurutkan menampilkan data urut 
 print("data setelah diurutkan:") 
 print(sorted(list))
